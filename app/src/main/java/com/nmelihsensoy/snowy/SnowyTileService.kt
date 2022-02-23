d93fd678aea53e8f065e292b99e82de7e84e277f
nmelihsensoy <nmelihsensoy@gmail.com>
2022-02-23 05:47:02 +0300

    project created

---
+++ app/src/main/java/com/nmelihsensoy/snowy/SnowyTileService.kt
@@ -0,0 +1,14 @@
+package com.nmelihsensoy.snowy
+
+import android.service.quicksettings.TileService
+
+class SnowyTileService : TileService() {
+
+    override fun onClick() {
+        super.onClick()
+    }
+
+    override fun onStartListening() {
+        super.onStartListening()
+    }
+}
